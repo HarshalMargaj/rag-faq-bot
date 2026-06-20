54b46097c3463eb74d7dc6ef64609ba2b2dfaf99
Harshal Margaj <harshal.margaj12@gmail.com>
2026-06-20 13:32:03 +0530

    feat: add generate embedding function

---
+++ lib/embedding.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+	apiKey: process.env.OPENAI_API_KEY,
+});
+
+export async function getEmbeddings(text: string) {
+	const response = await openai.embeddings.create({
+		model: "text-embedding-3-small",
+		input: text,
+	});
+
+	return response.data[0].embedding;
+}
